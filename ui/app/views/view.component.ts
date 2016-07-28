ec74516ddecff42a83bcdba6526e2f4723f0c17a
Lawrence Thatcher <lwthatcher@msn.com>
2016-07-28 17:16:09 -0600

    adding view component stub

---
+++ ui/app/views/view.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'vat-view',
+    templateUrl: 'app/views/view.component.html'
+})
+export class ViewComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+
+}
