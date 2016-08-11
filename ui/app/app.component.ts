e62bfbc70bfb9d5b9b55d35cc576a6d73e7b348e
Lawrence Thatcher <lwthatcher@msn.com>
2016-08-11 14:27:32 -0600

    getting rid of extra meta-data in app.component that is now being handled by the module

--- ui/app/app.component.ts
+++ ui/app/app.component.ts
@@ -1,18 +1,12 @@
 import { Component } from '@angular/core';
-import { HTTP_PROVIDERS } from '@angular/http';
-import { ROUTER_DIRECTIVES } from '@angular/router';
-
-import { NavBarComponent } from './nav-bar.component';
 
 @Component({
     moduleId: module.id,
     selector: 'vat',
     template: `
         <vat-nav-bar [title]="title"></vat-nav-bar>
-        <router-outlet></router-outlet>`,
-    directives: [ROUTER_DIRECTIVES, NavBarComponent]
+        <router-outlet></router-outlet>`
 })
-
 export class AppComponent {
     title = 'VAT';
 }
